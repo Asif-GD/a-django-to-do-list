485bcc1fbab7724e6abf1163c417c290092dfabf
Asif-GD <asif.coach@gmail.com>
2022-08-25 22:19:17 +0530

    set up html templates
    - templates/main/base.html
    - templates/main/home.html

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from utils import get_db_handle
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Greetings! Asif Iqbal.")
+    return render(request, "main/home.html", {}, content_type="text/html")

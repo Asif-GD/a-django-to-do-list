628f168e8fb88dda9459fcd9e5c5844d2ee627de
Asif-GD <asif.coach@gmail.com>
2022-08-26 11:43:20 +0530

    Enabled sqlite3 database again
    - created superuser

--- admin/settings.py
+++ admin/settings.py
@@ -74,14 +74,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
 
 # Password validation

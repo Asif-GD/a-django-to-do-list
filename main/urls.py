a8820433b40e2c0843ef9752becced2c9e5f4324
Asif-GD <asif.coach@gmail.com>
2022-08-30 05:56:06 +0530

    removed the url for lists.
    - causes errors while accessing other urls of the app.

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("lists/", views.user_list, name="user_list"),
-    path("<list_pk>/", views.user_task, name="user_task"),
+    # path("<list_pk>/", views.user_task, name="user_task"),
     path("register/", views.create_user, name="register_user"),
     path("login/", views.login_user, name="login_user"),
     path("logout/", views.logout_user, name="logout_user"),
